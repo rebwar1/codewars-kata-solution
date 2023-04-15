34ca31275256d6fedf71f6a1925b41c74321999f
rebwar <rebwar1.azizi@gmail.com>
2023-04-15 23:46:42 +0100

    8-kyu/Find Maximum and Minimum Values of a List

---
+++ 8-kyu/Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,12 @@
+const min = list => Math.min(...list);
+const max = list => Math.max(...list);
+
+var min = function (list) {
+  list.sort((a, b) => a - b);
+  return list[0];
+};
+
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list[0];
+};
