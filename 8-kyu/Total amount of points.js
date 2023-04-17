2c95463df47b642bd1712bba49659fb9fc30135a
rebwar <rebwar1.azizi@gmail.com>
2023-04-17 23:47:35 +0100

    8-kyu/Total amount of points

---
+++ 8-kyu/Total amount of points.js
@@ -0,0 +1,17 @@
+const points = games =>
+  games.reduce((output, current) => {
+    return (output +=
+      current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0);
+  }, 0);
+
+function points(games) {
+  let total = 0;
+  games.map(game => {
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
+}
