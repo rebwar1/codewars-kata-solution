e36567e79cb9dd9ca0084b6b94a24767a96ce2a6
rebwar <rebwar1.azizi@gmail.com>
2023-04-27 21:34:20 +0100

    8-kyu/All Star Code Challenge #18.

---
+++ 8-kyu/All Star Code Challenge #18.js
@@ -0,0 +1,13 @@
+function strCount(str, letter) {
+  return str.split(letter).length - 1;
+}
+
+function strCount(str, letter) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == letter) count++;
+  }
+
+  return count;
+}
