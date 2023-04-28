6d42630798d4e2cdc2e9276a1cbd95b44ad7653e
rebwar <rebwar1.azizi@gmail.com>
2023-04-28 21:29:22 +0100

    8-kyu/ Cat years, Dog years

---
+++ 8-kyu/ Cat years, Dog years.js
@@ -0,0 +1,11 @@
+var humanYearsCatYearsDogYears = function (y) {
+  if (y == 1) return [1, 15, 15];
+  if (y == 2) return [2, 24, 24];
+  return [y, (y - 2) * 4 + 24, (y - 2) * 5 + 24];
+};
+
+const humanYearsCatYearsDogYears = humanYears => [
+  humanYears,
+  (humanYears - 1 ? 16 : 11) + 4 * humanYears,
+  (humanYears - 1 ? 14 : 10) + 5 * humanYears,
+];
