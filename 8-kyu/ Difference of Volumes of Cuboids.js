d453c88f49f52845345c771f7d41a5258ef4669a
rebwar <rebwar1.azizi@gmail.com>
2023-05-03 21:24:10 +0100

    Difference of Volumes of Cuboids

---
+++ 8-kyu/ Difference of Volumes of Cuboids.js
@@ -0,0 +1,14 @@
+function find_difference(a, b) {
+  return Math.abs(
+    a.reduce((previous, current) => previous * current) -
+      b.reduce((previous, current) => previous * current)
+  );
+}
+
+function volume(c) {
+  return c.reduce((x, y) => x * y);
+}
+
+function find_difference(a, b) {
+  return Math.abs(volume(a) - volume(b));
+}
