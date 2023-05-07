48982c0edef0d13ffd448795bcc65c50154c5820
rebwar <rebwar1.azizi@gmail.com>
2023-05-07 23:49:52 +0100

    Reverse List Order

---
+++ 8-kyu/Reverse List Order.js
@@ -0,0 +1,11 @@
+function reverseList(list) {
+  return list.reverse();
+}
+
+function reverseList(list) {
+  var array = [];
+  for (var i = list.length - 1; i >= 0; i--) {
+    array.push(list[i]);
+  }
+  return array;
+}
