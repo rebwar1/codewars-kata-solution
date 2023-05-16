48374a4b08d145d66465be70b28c3a0c9334018d
rebwar <rebwar1.azizi@gmail.com>
2023-05-16 22:27:58 +0100

    8-kyu/Remove duplicates from list

---
+++ 8-kyu/Remove duplicates from list.js
@@ -0,0 +1,14 @@
+function distinct(a) {
+  return [...new Set(a)];
+}
+
+function distinct(arr) {
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!res.includes(arr[i])) {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+}
