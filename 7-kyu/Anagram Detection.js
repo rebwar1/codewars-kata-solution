dce978e7852da3baaff8bf93d15816a211c95eed
rebwar <rebwar1.azizi@gmail.com>
2023-06-05 22:21:28 +0100

    7-kyu/Anagram Detection

---
+++ 7-kyu/Anagram Detection.js
@@ -0,0 +1,5 @@
+var isAnagram = function (test, original) {
+  var t = test.toLowerCase().split("").sort().join("");
+  var o = original.toLowerCase().split("").sort().join("");
+  return t == o ? true : false;
+};
