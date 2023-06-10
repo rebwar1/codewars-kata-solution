4770ede1ec3e489a6f6f7edb78888516d2b5cbbe
rebwar <rebwar1.azizi@gmail.com>
2023-06-10 22:27:14 +0100

    7-kyu/Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer

---
+++ 7-kyu/Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer.js
@@ -0,0 +1,6 @@
+function getFirstPython(list) {
+  const dev = list.find(x => x.language === "Python");
+  return dev
+    ? `${dev.firstName}, ${dev.country}`
+    : "There will be no Python developers";
+}
