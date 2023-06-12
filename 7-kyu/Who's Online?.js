57db1f1b14a560eaa10192f4b504f8388f961846
rebwar <rebwar1.azizi@gmail.com>
2023-06-12 21:52:10 +0100

    7-kyu/Who's Online?

---
+++ 7-kyu/Who's Online?.js
@@ -0,0 +1,14 @@
+const whosOnline = friends => {
+  let output = {};
+  friends.forEach(e => {
+    let status = e.status;
+    if (status === "online" && e.lastActivity > 10) status = "away";
+    let temp = output[status];
+    if (output[status]) {
+      output[status].push(e.username);
+    } else {
+      output[status] = [e.username];
+    }
+  });
+  return output;
+};
