2aac6196312255dfcb36d07730e9c546a8c23035
rebwar <rebwar1.azizi@gmail.com>
2023-06-17 20:56:06 +0100

    7-kyu/Debug Basic Calculator

---
+++ 7-kyu/Debug Basic Calculator.js
@@ -0,0 +1,14 @@
+const calculate = (a, op, b) => {
+  switch (op) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return b ? a / b : null;
+  }
+
+  return null;
+};
